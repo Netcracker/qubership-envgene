99722c192e76f941ca50931b142eb9e08b23a1e3
Kamelov Nurlybek <kamelovn@gmail.com>
2025-02-18 14:49:29 +0500

    fix

--- build_gcip/pipegene_plugins/per_env/stub_job/main.py
+++ build_gcip/pipegene_plugins/per_env/stub_job/main.py
@@ -1,8 +1,8 @@
 from plugin_engine import PluginCore
 
 class ImportJobPlugin(PluginCore):
-    def __init__(self, params, pipeline_helper, pipeline) -> None:
-        super().__init__(params, pipeline_helper, pipeline)
+    def __init__(self) -> None:
+        super().__init__()
 
     def invoke(self) -> None:
         pass  # This is a stub that does nothing
