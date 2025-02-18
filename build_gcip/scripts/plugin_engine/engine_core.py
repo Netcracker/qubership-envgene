abc16bf66b3f6a12541ce9942075a52b689885ad
Kamelov Nurlybek <kamelovn@gmail.com>
2025-02-18 15:55:34 +0500

    fix

--- build_gcip/scripts/plugin_engine/engine_core.py
+++ build_gcip/scripts/plugin_engine/engine_core.py
@@ -1,11 +1,9 @@
-from typing import Callable
-
 from envgenehelper import logger
 from .usecase import PluginUseCase
 
 class PluginEngine:
     def __init__(self, plugins_dir: str, **kwargs) -> None:
-        self.plugins_to_invoke = [Callable]
+        self.plugins_to_invoke = []
         self.modules = PluginUseCase(plugins_dir).discover_plugins(True)
         for module in self.modules:
             registered_plugin = module(**kwargs)
