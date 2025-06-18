3983adfe4c122d3616aa69f97c0e59fb1b92707c
popoveugene <evgenii.a.popov.qubership@gmail.com>
2025-06-18 03:22:48 +0300

    test: wip

--- scripts/build_env/test_render_envs.py
+++ scripts/build_env/test_render_envs.py
@@ -10,7 +10,7 @@ test_data = [
       # ("cluster-01", "env-01", "composite-prod"),
       # ("cluster-01", "env-02", "composite-dev"),
       # ("cluster-01", "env-03", "composite-dev"),
-      # ("cluster-01", "env-04", "simple"),
+      ("cluster-01", "env-04", "simple"),
       ("cluster01", "env01", "test-01")
 ]
 
