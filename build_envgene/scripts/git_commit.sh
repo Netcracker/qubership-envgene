a774a0b900ac0418baf1e605b2dc5d04fbedfdb3
Tesma Jose <tesma.jose@netcracker.com>
2025-06-25 18:52:42 +0530

    added echo to debug

--- build_envgene/scripts/git_commit.sh
+++ build_envgene/scripts/git_commit.sh
@@ -161,6 +161,7 @@ if [ -e /tmp/gitlab-ci ]; then
 
   message="${ticket_id} [ci_build_parameters] Update gitlab-ci configurations"
 fi
+
 echo "Deployment session id is ${DEPLOYMENT_SESSION_ID}"
 if [ -n "${DEPLOYMENT_SESSION_ID}" ]; then
     echo "Deployment session id is ${DEPLOYMENT_SESSION_ID}"
@@ -168,6 +169,8 @@ if [ -n "${DEPLOYMENT_SESSION_ID}" ]; then
     echo "Appended commit message with session id"
 fi
 
+
+echo "Deployment session id is $DEPLOYMENT_SESSION_ID"
 echo "Checking changes..."
 git add ./*
 diff_status=0
