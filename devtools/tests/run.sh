64eaab5259a2b35b637bfc4e22c0064b595b7565
GlimmerCape <kamelovnn@gmail.com>
2025-08-27 16:56:59 +0500

    fix

--- devtools/tests/run.sh
+++ devtools/tests/run.sh
@@ -4,10 +4,10 @@ set -euxo
 cd "$CI_PROJECT_DIR"
 
 # Run tests
-# cd python/envgene/envgenehelper
-# pytest --capture=no -W ignore::DeprecationWarning --junitxml=../../../junit.xml
-# cd ../../..
-# mv junit.xml junit_envgenehelper.xml
+cd python/envgene/envgenehelper
+pytest --capture=no -W ignore::DeprecationWarning --junitxml=../../../junit.xml
+cd ../../..
+mv junit.xml junit_envgenehelper.xml
 
 cd scripts/build_env
 pytest --capture=no -W ignore::DeprecationWarning --junitxml=../../junit.xml
@@ -15,4 +15,4 @@ cd ../..
 mv junit.xml junit_build_env.xml
 
 # Merge results
-# python -m junitparser merge junit_build_env.xml junit_envgenehelper.xml junit.xml
+python -m junitparser merge junit_build_env.xml junit_envgenehelper.xml junit.xml
