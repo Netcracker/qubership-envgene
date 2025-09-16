297aee9197e1aef67dc856d5f32da1330007aec4
basa0425 <basudev.sahoo@netcracker.com>
2025-09-16 16:44:47 +0530

    feat: update runner timeout

--- build_pipegene/scripts/pipeline_helper.py
+++ build_pipegene/scripts/pipeline_helper.py
@@ -33,7 +33,8 @@ class JobExtended(Job):
         return job_data
 
 def job_instance(params, vars, needs=None, rules=None):
-    timeout = params.get('timeout', '10m')
+    #timeout = params.get('timeout', '10m')
+    timeout = os.getenv('RUNNER_SCRIPT_TIMEOUT', '10m')
     gitlab_runner_tag = vars.get('GITLAB_RUNNER_TAG_NAME')
     job = JobExtended(
         name=params['name'],
