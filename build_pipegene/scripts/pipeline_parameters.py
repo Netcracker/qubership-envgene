1c88cf079ca685d9999a1f2c90e69ef27bf2f119
GlimmerCape <kamelovnn@gmail.com>
2025-10-22 14:58:17 +0500

    fix: add session id handling

--- build_pipegene/scripts/pipeline_parameters.py
+++ build_pipegene/scripts/pipeline_parameters.py
@@ -1,5 +1,6 @@
 from os import getenv
 from pprint import pformat
+import uuid
 
 from envgenehelper.plugin_engine import PluginEngine
 
@@ -31,6 +32,7 @@ def get_pipeline_parameters() -> dict:
         'NS_BUILD_FILTER': getenv("NS_BUILD_FILTER", ""),
         'GITLAB_RUNNER_TAG_NAME' : getenv("GITLAB_RUNNER_TAG_NAME", ""),
         'RUNNER_SCRIPT_TIMEOUT' : getenv("RUNNER_SCRIPT_TIMEOUT") or "10m",
+        "DEPLOYMENT_SESSION_ID": getenv("DEPLOYMENT_SESSION_ID") or str(uuid.uuid4()),
     }
 
 class PipelineParametersHandler:
