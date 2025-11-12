0367f91ed37c5b7d544174fa03611ac0f7453e6c
andyroode <andrei.rudchenko.qubership@gmail.com>
2025-11-12 10:21:23 +0300

    chore: Updated gsf

--- gsf_packages/envgene_instance_project/git-system-follower-package/scripts/init.py
+++ gsf_packages/envgene_instance_project/git-system-follower-package/scripts/init.py
@@ -1,6 +1,6 @@
 from git_system_follower.develop.api.types import Parameters
 from git_system_follower.develop.api.cicd_variables import CICDVariable, create_variable
-from git_system_follower.develop.api.templates import create_template
+from git_system_follower.develop.api.templates import create_template, update_template, get_template_names
 
 def main(parameters: Parameters):
     templates = get_template_names(parameters)
@@ -17,4 +17,7 @@ def main(parameters: Parameters):
 
     variables = parameters.extras.copy()
     variables.pop('TEMPLATE', None)
-    create_template(parameters, template, variables)
+    if parameters.used_template:
+        update_template(parameters, variables, is_force=True)
+    else:
+        create_template(parameters, template, variables)
