25bd223a095ae4506f7a0ff2d2396980a236e19a
Dias <raibekovdias@gmail.com>
2025-11-27 16:18:33 +0500

    feat: centralise logs

--- python/envgene/envgenehelper/logger.py
+++ python/envgene/envgenehelper/logger.py
@@ -1,36 +1,25 @@
 from os import getenv
-import logging
+from loguru import logger
+import sys
 
-class CustomFormatter(logging.Formatter):
+log_level_str = getenv("ENVGENE_LOG_LEVEL", "INFO").upper()
+fmt = "<level>{time:YYYY-MM-DD HH:mm:ss,SSS} [{level: <8}] {message} [{name}:{line}]</level>"
 
-    grey = "\x1b[38;20m"
-    yellow = "\x1b[33;20m"
-    red = "\x1b[31;20m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
-    format = u'[%(asctime)s] [%(levelname)-8s] %(message)s [%(filename)s:%(lineno)d]'
+LEVEL_COLORS = {
+    "DEBUG": "<grey>{message}</grey>",
+    "INFO": "<white>{message}</white>",
+    "WARNING": "<yellow>{message}</yellow>",
+    "ERROR": "<red>{message}</red>",
+    "CRITICAL": "<bold red>{message}</bold red>"
+}
 
-    FORMATS = {
-        logging.DEBUG: grey + format + reset,
-        logging.INFO: grey + format + reset,
-        logging.WARNING: yellow + format + reset,
-        logging.ERROR: red + format + reset,
-        logging.CRITICAL: bold_red + format + reset
-    }
+logger.remove()
 
-    def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
-        return formatter.format(record)
+logger.add(
+    sys.stdout,
+    level=log_level_str,
+    colorize=True,
+    format=fmt
+)
 
-# create logger with 'spam_application'
-logger = logging.getLogger("envgene")
-logger.setLevel(logging.INFO)
-# create console handler with a higher log level
-ch = logging.StreamHandler()
-# get logging level from env var
-log_level_str = getenv('ENVGENE_LOG_LEVEL', 'INFO').upper()
-log_level = getattr(logging, log_level_str, logging.INFO)
-ch.setLevel(log_level)
-ch.setFormatter(CustomFormatter())
-logger.addHandler(ch)
+logger = logger.bind(name="envgene")
