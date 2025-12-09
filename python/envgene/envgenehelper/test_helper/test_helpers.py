61b822b4fb7b26f5032b1925313882c91f17aa53
GlimmerCape <kamelovnn@gmail.com>
2025-12-09 18:13:31 +0500

    fix: separate assertion and comparison into separate functions

--- python/envgene/envgenehelper/test_helper/test_helpers.py
+++ python/envgene/envgenehelper/test_helper/test_helpers.py
@@ -18,19 +18,15 @@ class TestHelpers:
         return path
 
     @staticmethod
-    def assert_dirs_content(source_dir, target_dir, missed_files=False, extra_files=False):
+    def compare_dirs_content(source_dir, target_dir) -> tuple[set[str],set[str],list,list]:
         source_map = {Path(f).name: f for f in get_all_files_in_dir(source_dir)}
         target_map = {Path(f).name: f for f in get_all_files_in_dir(target_dir)}
 
         source_files = set(source_map.keys())
         target_files = set(target_map.keys())
 
-        if extra_files:
-            extra_files = target_files - source_files
-            assert not extra_files, f"Extra files in target: {dump_as_yaml_format([target_map[name] for name in extra_files])}"
-        if missed_files:
-            missing_files = source_files - target_files
-            assert not missing_files, f"Missing files in target: {dump_as_yaml_format([source_map[name] for name in missing_files])}"
+        extra_files = target_files - source_files
+        missing_files = source_files - target_files
 
         files_to_compare = get_all_files_in_dir(source_dir)
         match, mismatch, errors = filecmp.cmpfiles(source_dir, target_dir, files_to_compare, shallow=False)
@@ -41,14 +37,20 @@ class TestHelpers:
                 file1 = os.path.join(source_dir, file)
                 file2 = os.path.join(target_dir, file)
                 with open(file1, 'r') as f1, open(file2, 'r') as f2:
-                    diff = difflib.unified_diff(
-                        f1.readlines(),
-                        f2.readlines(),
-                        fromfile=file1,
-                        tofile=file2,
-                        lineterm=''
-                    )
+                    diff = difflib.unified_diff(f1.readlines(), f2.readlines(), fromfile=file1, tofile=file2, lineterm='')
                     logger.error(f"Diff for {file}:\n{''.join(diff)}")
+
+        return extra_files, missing_files, mismatch, errors
+
+    @staticmethod
+    def assert_dirs_content(source_dir, target_dir, missed_files=False, extra_files=False):
+        extra_files, missing_files, mismatch, errors = TestHelpers.compare_dirs_content(source_dir, target_dir)
+
+        if extra_files:
+            assert not extra_files, f"Extra files in target: {dump_as_yaml_format([target_map[name] for name in extra_files])}"
+        if missed_files:
+            assert not missing_files, f"Missing files in target: {dump_as_yaml_format([source_map[name] for name in missing_files])}"
 
         assert not mismatch, f"Mismatched files: {dump_as_yaml_format(mismatch)}"
         assert not errors, f"Errors: {dump_as_yaml_format(errors)}"
+
