a38941fb11fbe21b933d8c673f0cbe85d0d03405
Dias <raibekovdias@gmail.com>
2025-12-11 15:44:08 +0500

    feat: update exception handle

--- python/envgene/envgenehelper/__main__.py
+++ python/envgene/envgenehelper/__main__.py
@@ -2,8 +2,7 @@ import sys
 from .logger import logger
 
 def handle_exception(exc_type, exc_value, exc_traceback):
-    logger.error("Uncaught exception:", exc_info=(exc_type, exc_value, exc_traceback))
     logger.opt(exception=(exc_type, exc_value, exc_traceback)).error(
-        f"Uncaught {exc_type.__name__}: {exc_value}"
+        f"{exc_type.__name__}: {exc_value}"
     )
 sys.excepthook = handle_exception
