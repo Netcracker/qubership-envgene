099ec52f139a76df50ec3d15634160e72597955a
Dias <raibekovdias@gmail.com>
2025-12-11 14:59:02 +0500

    feat: test-3

--- python/envgene/envgenehelper/__main__.py
+++ python/envgene/envgenehelper/__main__.py
@@ -3,5 +3,7 @@ from .logger import logger
 
 def handle_exception(exc_type, exc_value, exc_traceback):
     logger.error("Uncaught exception:", exc_info=(exc_type, exc_value, exc_traceback))
-
+    logger.opt(exception=(exc_type, exc_value, exc_traceback)).error(
+        f"Uncaught {exc_type.__name__}: {exc_value}"
+    )
 sys.excepthook = handle_exception
