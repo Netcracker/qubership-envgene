801c90834a8bf6b7e82004857becbad68ac14e54
Dias <raibekovdias@gmail.com>
2025-12-11 18:58:50 +0500

    feat: delete extra lines

--- python/envgene/envgenehelper/logger.py
+++ python/envgene/envgenehelper/logger.py
@@ -5,14 +5,6 @@ import sys
 log_level_str = getenv("LOG_LEVEL", "INFO").upper()
 fmt = "<level>{time:YYYY-MM-DD HH:mm:ss,SSS} [{level: <8}] {message} [{name}:{line}]</level>"
 
-LEVEL_COLORS = {
-    "DEBUG": "<grey>{message}</grey>",
-    "INFO": "<white>{message}</white>",
-    "WARNING": "<yellow>{message}</yellow>",
-    "ERROR": "<red>{message}</red>",
-    "CRITICAL": "<bold red>{message}</bold red>"
-}
-
 logger.remove()
 
 logger.add(
