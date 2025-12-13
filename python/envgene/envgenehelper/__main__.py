e63c662718b5789c6cf2894d654822a160912751
Dias <raibekovdias@gmail.com>
2025-12-13 15:48:38 +0500

    feat: centralise logs

--- python/envgene/envgenehelper/__main__.py
+++ python/envgene/envgenehelper/__main__.py
@@ -3,9 +3,9 @@ from .logger import logger
 
 def handle_exception(exc_type, exc_value, exc_traceback):
     if issubclass(exc_type, ReferenceError):
-        logger.critical(f"{exc_type.__name__}: {exc_value}")
+        logger.error(f"{exc_type.__name__}: {exc_value}")
         return
     else:
-        logger.critical("Uncaught exception:", exc_info=(exc_type, exc_value, exc_traceback))
+        logger.error("Uncaught exception:", exc_info=(exc_type, exc_value, exc_traceback))
 
 sys.excepthook = handle_exception
