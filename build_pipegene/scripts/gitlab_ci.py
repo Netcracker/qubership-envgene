5de81c2dbe68f0c58618f57b83692337064da9e8
GlimmerCape <kamelovnn@gmail.com>
2025-12-24 18:31:53 +0500

    fix: test

--- build_pipegene/scripts/gitlab_ci.py
+++ build_pipegene/scripts/gitlab_ci.py
@@ -145,10 +145,12 @@ def build_pipeline(params: dict):
     # check out repo only once in the first job of the generated pipeline, later jobs get it through artifacts from each other
     # purpose: avoid later jobs restoring files that were removed by previous jobs, so git commit job can commit those deletions
     for job in sorted_pipeline.find_jobs(JobFilter()): # gets all jobs in pipeline
-        job.artifacts.add_paths(".")
         if job.needs is None or len(job.needs) == 0:
             job.add_variables(GIT_STRATEGY="fetch")
+            job.scripts.append('mkdir -p "${{CI_PROJECT_DIR}}-empty" && cp -r "$CI_PROJECT_DIR"/. "${{CI_PROJECT_DIR}}-empty/"')
+            job.artifacts.add_paths("${CI_PROJECT_DIR}-empty")
         else:
             job.add_variables(GIT_STRATEGY="empty")
+            job.artifacts.add_paths("${CI_PROJECT_DIR}")
 
     sorted_pipeline.write_yaml()
