1686617f85b95adc62feb51d37786dfd999d5278
Andrei Rudchenko <andrei.rudchenko.qubership@gmail.com>
2026-01-15 09:59:41 +0300

    feat: Reverted the changes

--- scripts/build_env/env_template/process_env_template.py
+++ scripts/build_env/env_template/process_env_template.py
@@ -61,7 +61,7 @@ def download_artifact_new_logic(env_definition: dict) -> str:
     template_url = None
 
     resolved_version = app_version
-    dd_artifact_info = asyncio.run(artifact.check_artifact_async(app_def, FileExtension.JSON, app_version))
+    dd_artifact_info = asyncio.run(artifact.check_artifact_async(app_def, FileExtension.JSON, app_version, cred))
     if dd_artifact_info:
         logger.info("Loading environment template artifact info from deployment descriptor...")
         dd_url, dd_repo = dd_artifact_info
@@ -81,7 +81,7 @@ def download_artifact_new_logic(env_definition: dict) -> str:
     else:
         logger.info("Loading environment template artifact from zip directly...")
         group_id, artifact_id, version = app_def.group_id, app_def.artifact_id, app_version
-        artifact_info = asyncio.run(artifact.check_artifact_async(app_def, FileExtension.ZIP, app_version))
+        artifact_info = asyncio.run(artifact.check_artifact_async(app_def, FileExtension.ZIP, app_version, cred))
         if artifact_info:
             template_url, _ = artifact_info
         if "-SNAPSHOT" in app_version:
