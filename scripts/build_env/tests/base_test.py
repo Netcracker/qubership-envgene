1dcdedfae54a1d82a4681acbd5abe0c0f6aedc99
miyamuraga <198181742+miyamuraga@users.noreply.github.com>
2026-01-31 00:34:26 +0500

    test: add tests

--- scripts/build_env/tests/base_test.py
+++ scripts/build_env/tests/base_test.py
@@ -5,5 +5,10 @@ from pathlib import Path
 class BaseTest:
     base_dir = Path(__file__).resolve().parents[3]
     test_data_dir = base_dir / "test_data"
-    os.environ['CI_PROJECT_DIR'] = str(test_data_dir)
-    os.environ['JSON_SCHEMAS_DIR'] = str(Path(__file__).resolve().parents[3] / "schemas")
+    ci_project_dir = test_data_dir
+    os.environ['JSON_SCHEMAS_DIR'] = str(base_dir / "schemas")
+
+    def set_project_dir(self, *subdirs) -> Path:
+        self.ci_project_dir = self.ci_project_dir.joinpath(*subdirs)
+        os.environ['CI_PROJECT_DIR'] = str(self.ci_project_dir)
+        return self.ci_project_dir
--- scripts/build_env/tests/env_inventory_generation/test_env_inv_generation.py
+++ scripts/build_env/tests/env_inventory_generation/test_env_inv_generation.py
@@ -1,23 +1,21 @@
 from os import environ
 
-import pytest
-from envgenehelper import parse_env_names, get_cluster_name_from_full_name, dumpYamlToStr, \
-    get_environment_name_from_full_name, readYaml
+from envgenehelper import get_cluster_name_from_full_name, dumpYamlToStr, \
+    get_environment_name_from_full_name, readYaml, cleanup_dir
 
 from scripts.build_env.env_inventory_generation import generate_env_new_approach, Place
 from scripts.build_env.jinja.jinja import create_jinja_env
 from scripts.build_env.tests.base_test import BaseTest
 
-test_data = [
-    ("cluster-01/env-01", "", "")
-]
+FEATURE_TEST_DIR = "test_inventory_generation"
 
 
 class TestEnvInvGen(BaseTest):
-    @pytest.mark.parametrize("env_names, env_inventory_content_path, env_template_version", test_data)
-    def test_generate_env_create(self, env_names, env_inventory_content_path, env_template_version):
-        g_inventory_dir = self.test_data_dir / "test_inventory_generation"
-        full_env_name = parse_env_names(env_names)[0]
+
+    def test_generate_env_create(self):
+        self.set_project_dir(FEATURE_TEST_DIR, "output")
+        cleanup_dir(self.ci_project_dir)
+        full_env_name = "cluster-01/env-01"
         env_name = get_environment_name_from_full_name(full_env_name)
         cluster = get_cluster_name_from_full_name(full_env_name)
 
@@ -28,10 +26,10 @@ class TestEnvInvGen(BaseTest):
         places = [p.value for p in Place]
         action = "create_or_replace"
 
-        template = create_jinja_env(g_inventory_dir).get_template("input_content.yml.j2")
+        template = create_jinja_env(self.test_data_dir / FEATURE_TEST_DIR / "input").get_template(
+            "content.yml.j2")
         content = readYaml(text=template.render(places=places, action=action, env=env_name, cluster=cluster),
                            safe_load=True)
         environ['ENV_INVENTORY_CONTENT'] = dumpYamlToStr(content)
 
         generate_env_new_approach()
-
