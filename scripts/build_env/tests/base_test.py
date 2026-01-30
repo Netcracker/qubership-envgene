a58db56d6863907a267f761fb612ffe5135c9b61
miyamuraga <198181742+miyamuraga@users.noreply.github.com>
2026-01-31 00:38:50 +0500

    test: add tests

--- scripts/build_env/tests/base_test.py
+++ scripts/build_env/tests/base_test.py
@@ -6,6 +6,7 @@ class BaseTest:
     base_dir = Path(__file__).resolve().parents[3]
     test_data_dir = base_dir / "test_data"
     ci_project_dir = test_data_dir
+    os.environ['CI_PROJECT_DIR'] = str(test_data_dir)
     os.environ['JSON_SCHEMAS_DIR'] = str(base_dir / "schemas")
 
     def set_project_dir(self, *subdirs) -> Path:
