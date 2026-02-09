fc9d74c6a6797f448e63e595c3d92267a72cc1f1
miyamuraga <198181742+miyamuraga@users.noreply.github.com>
2026-02-09 20:52:54 +0500

    feat: Implement new pipeline parameter "ENV_TEMPLATE_VERSION_UPDATE_MODE"

--- scripts/build_env/env_template/set_template_version.py
+++ scripts/build_env/env_template/set_template_version.py
@@ -4,15 +4,15 @@ from pathlib import Path
 from envgenehelper import beautifyYaml, writeYamlToFile, logger, getenv_with_error, getEnvDefinitionPath
 from envgenehelper import getEnvDefinition
 
-from envgenehelper.models import EnvTemplateVersionUpdateMode
+from envgenehelper.models import TemplateVersionUpdateMode
 
 
-def update_version(env_definition_dir, version_to_add, update_mode: EnvTemplateVersionUpdateMode):
+def update_version(env_definition_dir, version_to_add, update_mode: TemplateVersionUpdateMode):
     env_definition_path = getEnvDefinitionPath(env_definition_dir)
     logger.info(f"Started version update to {version_to_add} in {env_definition_path}.")
     data = getEnvDefinition(env_instances_dir)
 
-    if update_mode == EnvTemplateVersionUpdateMode.TEMPORARY:
+    if update_mode == TemplateVersionUpdateMode.TEMPORARY:
         logger.info("Template update mode: TEMPORARY, Skip updating template artifact version in env_definition.yml")
         return
     if ":" in version_to_add:
@@ -44,5 +44,5 @@ if __name__ == "__main__":
     environment_name = getenv_with_error("ENVIRONMENT_NAME")
     env_instances_dir = Path(f"{base_dir}/environments/{cluster_name}/{environment_name}")
     version_to_add = getenv("ENV_TEMPLATE_VERSION")
-    env_tmp_ver_update_mode = EnvTemplateVersionUpdateMode(getenv("ENV_TEMPLATE_VERSION_UPDATE_MODE"))
+    env_tmp_ver_update_mode = TemplateVersionUpdateMode(getenv("ENV_TEMPLATE_VERSION_UPDATE_MODE"))
     update_version(env_instances_dir, version_to_add, env_tmp_ver_update_mode)
