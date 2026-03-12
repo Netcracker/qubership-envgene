3af0be0c234b46493617c89e811b9635e3c23ad5
chethana-shastry-p <p.chethana@gmail.com>
2026-03-12 12:40:58 +0530

    fix: adding new artifact path in sd job

--- build_pipegene/scripts/process_sd_job.py
+++ build_pipegene/scripts/process_sd_job.py
@@ -1,4 +1,4 @@
-from os import getenv
+from os
 
 from gcip import WhenStatement
 
@@ -40,6 +40,11 @@ def prepare_process_sd(pipeline, full_env, environment_name, cluster_name, artif
     }
 
     process_sd_job = job_instance(params=process_sd_set_params, vars=process_sd_set_vars)
+
+    tmp_path = os.path.join(os.environ["CI_PROJECT_DIR"], os.environ["CI_PIPELINE_ID"], "tmp")
+    if os.path.isdir(tmp_path):
+        process_sd_job.artifacts.add_paths(tmp_path)  
+
     
     process_sd_job.artifacts.add_paths("${CI_PROJECT_DIR}/environments/" + full_env)
     process_sd_job.artifacts.when = WhenStatement.ALWAYS
