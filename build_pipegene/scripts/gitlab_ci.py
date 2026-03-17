5a540be4ae3a08dcd506b8ae4e07a4c0a19c810c
chethana-shastry-p <p.chethana@gmail.com>
2026-03-17 11:33:33 +0530

    fix: Bugfix/workdircleanup (#1125)
    * fix: adding git_clone_path
    
    * fix: correcting syntax
    
    * fix: cleaning git commands
    
    * fix: removing script of printing
    
    * fix: with flag and prints
    
    * fix: correcting syntax

--- build_pipegene/scripts/gitlab_ci.py
+++ build_pipegene/scripts/gitlab_ci.py
@@ -196,7 +196,7 @@ def build_pipeline(params: dict) -> None:
 
     sorted_pipeline.add_variables(
         #GIT_CLONE_PATH="$CI_BUILDS_DIR/$CI_PROJECT_PATH/$CI_PIPELINE_ID/$CI_JOB_ID",
-        GIT_CLONE_PATH="$CI_BUILDS_DIR/$CI_PROJECT_PATH/$CI_PIPELINE_ID/$CI_JOB_ID"
+        GIT_CLONE_PATH="$CI_BUILDS_DIR/$CI_PROJECT_PATH/$CI_PIPELINE_ID/$CI_JOB_ID",
         FF_ENABLE_JOB_CLEANUP="true"
     )# check out repo only once in the first job of the generated pipeline, later jobs get it through artifacts from each other
     # purpose: avoid later jobs restoring files that were removed by previous jobs, so git commit job can commit those deletions
