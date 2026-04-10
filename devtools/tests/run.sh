6474399314e998f3cdfcca355ebeebfe6a6feac2
GlimmerCape <kamelovnn@gmail.com>
2026-04-10 19:29:33 +0500

    fix: FAKE-000 fix

--- devtools/tests/run.sh
+++ devtools/tests/run.sh
@@ -19,12 +19,12 @@ run_pytest_suite() {
   mv "${CI_PROJECT_DIR}/junit.xml" "${CI_PROJECT_DIR}/junit_${name}.xml"
 }
 
-# run_pytest_suite envgenehelper python/envgene/envgenehelper
+run_pytest_suite envgenehelper python/envgene/envgenehelper
 run_pytest_suite pipegene build_pipegene/scripts
-# run_pytest_suite artifact_searcher python/artifact-searcher/artifact_searcher
-# run_pytest_suite bg_manage scripts/bg_manage
-# run_pytest_suite build_env scripts/build_env
-# run_pytest_suite cred_rotation creds_rotation/scripts
-# run_pytest_suite sbom_retention build_effective_set_generator/scripts
+run_pytest_suite artifact_searcher python/artifact-searcher/artifact_searcher
+run_pytest_suite bg_manage scripts/bg_manage
+run_pytest_suite build_env scripts/build_env
+run_pytest_suite cred_rotation creds_rotation/scripts
+run_pytest_suite sbom_retention build_effective_set_generator/scripts
 
 junitparser merge junit_*.xml junit.xml
