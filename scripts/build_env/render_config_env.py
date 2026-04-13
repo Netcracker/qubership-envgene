7d8522ea9291abe4be6c364689238ac514c0d76d
Vissapragada SriSatya Venkata Surya Ganesh <159680576+ganeshvissapragada@users.noreply.github.com>
2026-04-13 15:49:07 +0530

    feat: composite-structure-validation (#1227)
    * feat: update render_config_env for composite structure validation
    
    * feat: render composite structure before validation
    
    * feat: composite-structure-validation

--- scripts/build_env/render_config_env.py
+++ scripts/build_env/render_config_env.py
@@ -12,9 +12,11 @@ from pydantic import BaseModel, Field
 
 from jinja.jinja import create_jinja_env
 from jinja.replace_ansible_stuff import replace_ansible_stuff, escaping_quotation
+print("FILE LOADED:", __file__)
 
 SCHEMAS_DIR = Path(__file__).resolve().parents[2] / "schemas"
 TD_SCHEMA = str(SCHEMAS_DIR / "template-descriptor.schema.json")
+composite_SCHEMA = str(SCHEMAS_DIR / "composite-structure.schema.json")
 
 yml = create_yaml_processor()
 
@@ -425,7 +427,9 @@ class EnvGenerator:
             current_env_dir = self.ctx.current_env_dir
             cs_file = Path(current_env_dir) / "composite_structure.yml"
             cs_file.parent.mkdir(parents=True, exist_ok=True)
+            print("validation file loaded")
             self.render_from_file_to_file(Template(composite_structure).render(self.ctx.as_dict()), str(cs_file))
+            validate_yaml_by_scheme_or_fail(cs_file, composite_SCHEMA)
 
     def get_rendered_target_path(self, template_path: Path) -> Path:
         path_str = str(template_path)
