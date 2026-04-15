cd81ed7eae245570498334caefb8334146e135eb
miyamuraga <198181742+miyamuraga@users.noreply.github.com>
2026-04-15 20:38:47 +0500

    fix: align

--- build_effective_set_generator/scripts/sboms_retention_policy.py
+++ build_effective_set_generator/scripts/sboms_retention_policy.py
@@ -7,8 +7,12 @@ from envgenehelper.models import SbomRetentionConfig
 def sboms_retention_policy():
     work_dir = getenv_with_error('CI_PROJECT_DIR')
     sboms_dir = get_sboms_dir(work_dir)
-    config = get_envgene_config_yaml().get("sbom_retention")
 
+    if not sboms_dir.exists():
+        logger.warning(f"SBOM directory does not exist: {sboms_dir}")
+        return
+
+    config = get_envgene_config_yaml().get("sbom_retention")
     if not config:
         disabled = True
     else:
@@ -16,26 +20,26 @@ def sboms_retention_policy():
         disabled = not sbom_retention.enabled
 
     if disabled:
-        logger.info("SBOMs retention policy is disabled")
+        logger.info("SBOM retention policy is disabled")
         return
 
-    if not sboms_dir.exists():
-        logger.warning(f"There is no such directory: {sboms_dir}")
-        return
+    logger.info(f"SBOM retention policy is enabled for directory {sboms_dir}")
 
-    logger.info("SBOMs retention policy is enabled")
     for sbom_path in sboms_dir.iterdir():
         if sbom_path.is_file():
-            logger.info(f"Removing outdated format file: {sbom_path}")
+            logger.info(f"Removing legacy SBOM file: {sbom_path}")
             deleteFileIfExists(sbom_path)
 
     if sbom_retention.keep_versions_per_app:
         for app_sbom_dir in sboms_dir.iterdir():
+            logger.info(f"SBOM retention policy: keep_versions_per_app={sbom_retention.keep_versions_per_app}, "
+                        f"target_dir={app_sbom_dir}")
             cleanup_dir_by_age(app_sbom_dir, sbom_retention.keep_versions_per_app)
 
     if is_over_size_limit(sboms_dir, CI_JOB_ARTIFACT_MAX_SIZE_MB):
-        logger.info("Directory size above threshold, starting cleanup")
-        cleanup_dir_by_age(app_sbom_dir, 1)
+        logger.info(f"SBOM directory exceeds size limit, starting cleanup: {sboms_dir}")
+        for app_sbom_dir in sboms_dir.iterdir():
+            cleanup_dir_by_age(app_sbom_dir, 1)
 
 
 if __name__ == "__main__":
