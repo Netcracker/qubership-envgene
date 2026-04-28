d3ce92ea8d35b43dcfd23629d5f306a94b85205c
chethana-shastry-p <p.chethana@gmail.com>
2026-04-28 11:49:34 +0530

    fix: addressing review comments

--- build_effective_set_generator/effective-set-generator/src/test/java/org/qubership/cloud/devops/cli/utils/yaml/AdaptiveYamlTest.java
+++ build_effective_set_generator/effective-set-generator/src/test/java/org/qubership/cloud/devops/cli/utils/yaml/AdaptiveYamlTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024-2025 NetCracker Technology Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.qubership.cloud.devops.cli.utils.yaml;
 
 import org.junit.jupiter.api.Test;
@@ -17,9 +33,7 @@ public class AdaptiveYamlTest {
         root.put("a", Map.of("x", 1));
         root.put("b", Map.of("y", 2));
 
-        boolean result = AdaptiveYaml.shouldExpand(root);
-
-        assertFalse(result);
+        assertFalse(AdaptiveYaml.shouldExpand(root));
     }
 
     @Test
@@ -31,9 +45,7 @@ public class AdaptiveYamlTest {
         root.put("a", shared);
         root.put("b", shared);
 
-        boolean result = AdaptiveYaml.shouldExpand(root);
-
-        assertFalse(result);
+        assertFalse(AdaptiveYaml.shouldExpand(root));
     }
 
     @Test
@@ -47,8 +59,6 @@ public class AdaptiveYamlTest {
             list.add(shared);
         }
 
-        boolean result = AdaptiveYaml.shouldExpand(list);
-
-        assertTrue(result);
+        assertTrue(AdaptiveYaml.shouldExpand(list));
     }
 }
