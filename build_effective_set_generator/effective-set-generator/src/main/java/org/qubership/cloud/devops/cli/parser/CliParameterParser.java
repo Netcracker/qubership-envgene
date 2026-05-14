07f120151299dedc06bf88b2cdb61fe98b254753
GeethaGadde99 <geethagadde55@gmail.com>
2026-05-14 14:46:42 +0530

    fix: envgeneNullValue does not enforce mandatory override in environment-specific ParameterSet

--- build_effective_set_generator/effective-set-generator/src/main/java/org/qubership/cloud/devops/cli/parser/CliParameterParser.java
+++ build_effective_set_generator/effective-set-generator/src/main/java/org/qubership/cloud/devops/cli/parser/CliParameterParser.java
@@ -285,6 +285,7 @@ public class CliParameterParser {
                     originalNamespace,
                     k8TokenMap,
                     customParams);
+            validateParameterBundle(parameterBundle, tenantName, cloudName, originalNamespace);
             ParameterBundle cleanupParameterBundle = parametersServiceV2.getCleanupParameterBundle(tenantName, cloudName, namespaceName, null, originalNamespace, k8TokenMap);
             createCleanupParams(parameterBundle, cleanupParameterBundle);
         } else {
@@ -294,7 +295,7 @@ public class CliParameterParser {
                     appName,
                     deployerInputs,
                     originalNamespace);
-
+            validateParameterBundle(parameterBundle, tenantName, cloudName, originalNamespace);
         }
         createFiles(namespaceName, appName, parameterBundle, originalNamespace);
     }
@@ -391,5 +392,51 @@ public class CliParameterParser {
             throw new NotFoundException(String.format(ENTITY_NOT_FOUND, "Cloud"));
         }
     }
+
+    private void validateParameterBundle(ParameterBundle bundle,
+                                         String tenantName,
+                                         String cloudName,
+                                         String namespaceName) {
+        validateMap("tenant", tenantName, "deployParameters", bundle.getDeployParams());
+        validateMap("tenant", tenantName, "e2eParameters", bundle.getE2eParams());
+        validateMap("tenant", tenantName, "technicalConfigurationParameters", bundle.getConfigServerParams());
+
+        validateMap("cloud", cloudName, "deployParameters", bundle.getDeployParams());
+        validateMap("cloud", cloudName, "e2eParameters", bundle.getE2eParams());
+        validateMap("cloud", cloudName, "technicalConfigurationParameters", bundle.getConfigServerParams());
+
+        validateMap("namespace", namespaceName, "deployParameters", bundle.getDeployParams());
+        validateMap("namespace", namespaceName, "e2eParameters", bundle.getE2eParams());
+        validateMap("namespace", namespaceName, "technicalConfigurationParameters", bundle.getConfigServerParams());
+    }
+
+    private void validateMap(String entityType,
+                             String entityName,
+                             String paramType,
+                             Map<String, Object> params) {
+
+        if (MapUtils.isEmpty(params)) {
+            return;
+        }
+        params.forEach((key, value) -> {
+            if (isNullValue(value)) {
+                throw new IllegalStateException(
+                        String.format(
+                                "Error while validating parameters:%n  %s.%s.%s - is not set",
+                                entityName,
+                                paramType,
+                                key
+                        )
+                );
+            }
+        });
+    }
+
+    private boolean isNullValue(Object value) {
+        return value instanceof String &&
+                NULL_VALUE.equalsIgnoreCase((String) value);
+    }
+
+}
 
 }
