4c4e03fdd94f55136a2af8f2919cf97ee99f5198
miyamuraga <198181742+miyamuraga@users.noreply.github.com>
2026-06-12 16:32:11 +0500

    fix: 1 run

--- scripts/effective_set/effective_set_entrypoint.py
+++ scripts/effective_set/effective_set_entrypoint.py
@@ -18,7 +18,7 @@ def effective_set_entrypoint():
     sd_path = get_sd_dir().joinpath(SD_FILE_NAME)
     delta_sd_path = get_sd_dir().joinpath(DELTA_SD_FILE_NAME)
 
-    if get_es_generation_mode() == GenerationMode.PARTIAL:
+    if get_es_generation_mode() == GenerationMode.PARTIAL and delta_sd_path.is_file():
         if resolve_partial_merge_mode() == PartialMergeMode.REVERSE:
             _run_reverse_merge(effective_set_dir, delta_sd_path, sd_path)
         else:
