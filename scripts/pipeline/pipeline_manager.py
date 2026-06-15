0ec5b95fe1082735826324d2d6663c5e047393f5
miyamuraga <198181742+miyamuraga@users.noreply.github.com>
2026-06-15 15:16:14 +0500

    feat: consolidate into into a single job

--- scripts/pipeline/pipeline_manager.py
+++ scripts/pipeline/pipeline_manager.py
@@ -1,4 +1,3 @@
-
 import click
 from orchestrator import PipelineParametersHandler
 
@@ -10,20 +9,13 @@ def pipeline():
 
 @pipeline.command("log_params")
 def log_params():
-    handler = PipelineParametersHandler()
+    handler = PipelineParametersHandler.from_env()
     handler.log_pipeline_params()
-
-
-# for setting in parameters for static pipeline
-@pipeline.command("init_params")
-def init_pipeline():
-    handler = PipelineParametersHandler()
-    handler.init_pipe_params()
 
 
 @pipeline.command("write_dotenv")
 def write_dotenv():
-    handler = PipelineParametersHandler()
+    handler = PipelineParametersHandler.from_env()
     handler.write_dotenv()
 
 
