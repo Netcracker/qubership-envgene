87589f35ed7e9f61566477d3568f7b98d0aeaa93
GlimmerCape <kamelovnn@gmail.com>
2026-06-19 13:20:44 +0500

    feat: sparse checkout for first job @cf_skip

---
+++ scripts/utils/sparse_checkout.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ "$#" -eq 0 ]; then
+  echo "Usage: sparse_checkout.sh <path> [<path> ...]" >&2
+  exit 1
+fi
+
+git config --global init.defaultBranch main
+git init .
+git remote add origin "${CI_REPOSITORY_URL}"
+git -c protocol.version=2 fetch --depth=1 origin \
+  "+refs/heads/${CI_COMMIT_REF_NAME}:refs/remotes/origin/${CI_COMMIT_REF_NAME}"
+git checkout --force "${CI_COMMIT_SHA}"
+git sparse-checkout init --cone
+git sparse-checkout set "$@"
+git read-tree -mu HEAD
