237c9eb0b3ed8d61c7c8ac74fe5fe14b8bbae084
Maerodrim <43945193+Maerodrim@users.noreply.github.com>
2026-06-22 12:47:28 +0500

    feat: Ref schemas and update credential schema path

--- scripts/tests/conftest.py
+++ scripts/tests/conftest.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+
 import envgenehelper.business_helper as business_helper
 
-business_helper.get_schema_dir = business_helper.get_schema_dir(2)
+
+def _test_get_schema_dir(level=2):
+    return Path(business_helper.__file__).resolve().parents[level] / "schemas"
+
+
+business_helper.get_schema_dir = _test_get_schema_dir
