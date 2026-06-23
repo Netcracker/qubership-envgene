3df3077786beb6999ca207e19df73f349dbe5b12
Maerodrim <stanislav.sergynin@gmail.com>
2026-06-23 17:00:52 +0500

    feat: Feature/imp (#1522)
    * feat: Modern toolset (#3)
    
    * feat: Ref schemas and update credential schema path
    
    * feat: Ref schemas and update credential schema path
    
    * feat: Ref schemas and update credential schema path
    
    * feat: Ref schemas and update credential schema path
    
    * feat: Ref schemas and update credential schema path
    
    * feat: Ref schemas and update credential schema path
    
    * feat: Ref schemas and update credential schema path
    
    * feat: Ref schemas and update credential schema path
    
    * feat: Ref schemas and update credential schema path
    
    * feat: Ref schemas and update credential schema path
    
    * feat: Ref schemas and update credential schema path
    
    * feat: Ref schemas and update credential schema path
    
    * feat: Ref schemas and update credential schema path
    
    * feat: Ref schemas and update credential schema path
    
    * feat: Ref schemas and update credential schema path
    
    * feat: Ref schemas and update credential schema path
    
    * feat: ref envgenehelper.business_helper.get_schema_dir
    
    * feat: improvements
    
    ---------
    
    Co-authored-by: stse0320 <stanislav.sergunin@netcracker.com>

--- scripts/tests/conftest.py
+++ scripts/tests/conftest.py
@@ -2,9 +2,4 @@ from pathlib import Path
 
 import envgenehelper.business_helper as business_helper
 
-
-def _test_get_schema_dir(level=3):
-    return Path(business_helper.__file__).resolve().parents[level] / "schemas"
-
-
-business_helper.get_schema_dir = _test_get_schema_dir
+business_helper.get_schema_dir = lambda: Path(__file__).resolve().parents[2] / "schemas"
