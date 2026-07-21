60971d831b04dadff55fc0123be06906e789d90e
chethana-shastry-p <p.chethana@gmail.com>
2026-07-21 17:54:59 +0530

    fix: Adding cred_rotation_payload key in pipeline_parameters

--- scripts/pipeline/pipeline_parameters.py
+++ scripts/pipeline/pipeline_parameters.py
@@ -71,6 +71,7 @@ class PipelineParametersHandler(BaseModel):
             "SSL_CERTIFICATES_BUNDLE": getenv("SSL_CERTIFICATES_BUNDLE"),
             "NAMESPACE_NAMES": getenv("NAMESPACE_NAMES", ""),
             "APPLICATION_VERSIONS": getenv("APPLICATION_VERSIONS"),
+            "CRED_ROTATION_PAYLOAD": getenv("CRED_ROTATION_PAYLOAD"),
         }
 
         pipe_param_plugin = PluginEngine(plugins_dir='/module/scripts/plugins/pipe_parameters')
