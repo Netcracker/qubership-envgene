3b5b3bfe06758af592f76c37b48716666b249b19
chethana-shastry-p <p.chethana@gmail.com>
2026-07-23 12:49:59 +0530

    feat: Removing url attribute from secretStore

--- build_effective_set_generator/vals-reference-core/src/main/java/org/qubership/cloud/devops/vals/core/dto/SecretStoreDTO.java
+++ build_effective_set_generator/vals-reference-core/src/main/java/org/qubership/cloud/devops/vals/core/dto/SecretStoreDTO.java
@@ -29,7 +29,6 @@ import lombok.NoArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SecretStoreDTO {
     private SecretStoreType type;
-    private String url;
     private String mountPath; // Required when type is VAULT
     private String vaultName; // Required when type is AZURE
     private String region; // Required when type is AWS
