d87cede76d5bd396912b46ec72891a3d4ea0fa4b
chethana-shastry-p <p.chethana@gmail.com>
2026-07-23 12:49:59 +0530

    feat: Removing url attribute from secretStore

--- modules/envgene/envgenehelper/models.py
+++ modules/envgene/envgenehelper/models.py
@@ -46,7 +46,6 @@ class PipelineType(CaseInsensitiveEnum):
 
 class SecretStore(BaseModel):
     type: Literal["vault", "gcp", "aws", "azure"]
-    url: str
     projectId: Optional[str] = None
     mountPath: Optional[str] = None
     vaultName: Optional[str] = None
