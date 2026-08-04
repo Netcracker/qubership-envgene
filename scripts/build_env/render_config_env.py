b9ca546cda647433cb39e48e184983ecf868569e
GeethaGadde99 <geethagadde55@gmail.com>
2026-08-04 16:02:42 +0530

    feat: Computed `current_env.composite_topology` environment variable

--- scripts/build_env/render_config_env.py
+++ scripts/build_env/render_config_env.py
@@ -696,7 +696,7 @@ class EnvGenerator:
             self.ctx.current_env_dir = current_env_dir
             self.set_env_templates()
             self.generate_composite_structure()
-            self.compute_composite_topology()()
+            self.compute_composite_topology()
             self.generate_bgd_file()
             self.generate_solution_structure()
             self.generate_tenant_file()
