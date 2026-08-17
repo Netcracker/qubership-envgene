c49ac9f59b4d83fe4871fba0e67b60dee71749fb
chethana-shastry-p <p.chethana@gmail.com>
2026-08-17 11:37:22 +0530

    fix: adding external_cred_provision_cli call to run_gitlab_deploy_effective_set

--- build_effective_set_generator/effective-set-generator/src/main/java/org/qubership/cloud/devops/cli/repository/implementation/FileDataRepositoryImpl.java
+++ build_effective_set_generator/effective-set-generator/src/main/java/org/qubership/cloud/devops/cli/repository/implementation/FileDataRepositoryImpl.java
@@ -43,7 +43,6 @@ import org.qubership.cloud.devops.commons.pojo.consumer.Property;
 import org.qubership.cloud.devops.commons.pojo.credentials.dto.CredentialDTO;
 import org.qubership.cloud.devops.commons.pojo.credentials.model.CredentialsTypeEnum;
 import org.qubership.cloud.devops.commons.pojo.cs.CompositeStructureDTO;
-import org.qubership.cloud.devops.commons.pojo.extcreds.SecretStoreDTO;
 import org.qubership.cloud.devops.commons.pojo.namespaces.dto.NamespaceDTO;
 import org.qubership.cloud.devops.commons.pojo.namespaces.dto.NamespacePrefixDTO;
 import org.qubership.cloud.devops.commons.pojo.profile.dto.ProfileFullDto;
@@ -51,6 +50,7 @@ import org.qubership.cloud.devops.commons.pojo.registries.dto.RegistryDTO;
 import org.qubership.cloud.devops.commons.pojo.tenants.dto.TenantDTO;
 import org.qubership.cloud.devops.commons.repository.interfaces.FileDataConverter;
 import org.qubership.cloud.devops.commons.repository.interfaces.FileDataRepository;
+import org.qubership.cloud.devops.vals.core.dto.SecretStoreDTO;
 
 import java.io.File;
 import java.io.IOException;
