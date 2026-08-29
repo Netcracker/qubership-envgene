01f018c1da434f337ecef2467b142a4eeb257be1
Maerodrim <stanislav.sergynin@gmail.com>
2026-08-29 18:59:01 +0500

    Revert "test: add Credential Rotation BDD tests and supporting infrastructure"
    This reverts commit fce26c90efd3b7d8deebc9f172bf94466c06b822.

--- cucumber_tests/step_defs/test_credential_rotation.py
+++
@@ -1,6 +0,0 @@
-from pytest_bdd import scenarios
-from cucumber_tests.shared_steps.common_steps import *
-from cucumber_tests.shared_steps.unified_pipeline_steps import *
-from cucumber_tests.shared_steps.credential_rotation_steps import *
-
-scenarios('../features/credential-rotation.feature')
