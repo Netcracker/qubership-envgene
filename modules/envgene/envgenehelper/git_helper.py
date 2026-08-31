9ee5283f3c9ee48c2ed89184f75c11b129b42162
Dias <raibekovdias@gmail.com>
2026-08-31 17:21:17 +0500

    fix: sparse

--- modules/envgene/envgenehelper/git_helper.py
+++ modules/envgene/envgenehelper/git_helper.py
@@ -106,11 +106,10 @@ class GitRepoManager:
                 f"{self.ctx.server_host}/{self.ctx.project_path}.git"
             )
 
-    def _fetch(self, ref: str, checkout: str, checkout_option: list[str], create_remote: bool = False) -> None:
-        if create_remote:
-            self.repo.create_remote("origin", self._resolve_remote_url())
-        else:
-            self.repo.remote("origin").set_url(self._resolve_remote_url(), push=True)
+    def _fetch(self, ref: str, checkout: str, checkout_option: list[str]) -> None:
+        origin = self.repo.remote("origin")
+        origin.set_url(self._resolve_remote_url())
+        origin.set_url(self._resolve_remote_url(), push=True)
 
         try:
             logger.info(f"git fetch --depth=1 origin {ref}")
@@ -269,15 +268,7 @@ class GitRepoManager:
 
         retry_call(retry_policy, run, retry_on=(RuntimeError,))
 
-    def sparse_checkout(self, sparse_paths: list[str], *, fetch: bool = True) -> None:
-        if fetch:
-            self._fetch(
-                ref=self.ctx.commit_sha,
-                checkout=self.ctx.commit_sha,
-                checkout_option=["--force"],
-                create_remote=True,
-            )
-
+    def sparse_checkout(self, sparse_paths: list[str]) -> None:
         logger.info("git sparse-checkout init --cone")
         self.repo.git.sparse_checkout("init", "--cone")
         logger.info(f"git sparse-checkout set ({len(sparse_paths)} paths)")
