19c016f1a8963eb1618fd778ff3dc90837269dd4
GeethaGadde99 <geethagadde55@gmail.com>
2026-09-11 09:26:13 +0530

    fix: Sensitive Information Exposed in Effective Set Cleanup Parameters - added loggers

--- build_effective_set_generator/effective-set-generator/src/main/java/org/qubership/cloud/devops/cli/parser/CliParameterParser.java
+++ build_effective_set_generator/effective-set-generator/src/main/java/org/qubership/cloud/devops/cli/parser/CliParameterParser.java
@@ -451,9 +451,18 @@ public class CliParameterParser {
             Files.createDirectories(Path.of(runtimeNsDir));
             fileDataConverter.writeToFile(new HashMap<>(), deployNsDir, ".cleaned");
             fileDataConverter.writeToFile(new HashMap<>(), runtimeNsDir, ".cleaned");
-
+            LOGGER.warn(
+                    "SECURITY DEBUG: entering cleanup generation, namespace={}",
+                    namespaceName);
             // cleanup parameters
             ParameterBundle cleanupParameterBundle = parametersServiceV2.getCleanupParameterBundle(tenantName, cloudName, namespaceName, null, originalNamespace, k8TokenMap, getExtCredEntities());
+            LOGGER.warn(
+                    "SECURITY DEBUG: cleanup parameters keys={}",
+                    cleanupParameterBundle.getCleanupParameters().keySet());
+
+            LOGGER.warn(
+                    "SECURITY DEBUG: cleanup secure parameters keys={}",
+                    cleanupParameterBundle.getCleanupSecureParameters().keySet());
             createCleanupParams(cleanupParameterBundle);
 
             String cleanupDir = String.format("%s/%s/%s", sharedData.getOutputDir(), "cleanup", namespaceName);
